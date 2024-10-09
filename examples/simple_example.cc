ec28f601953168d4e1b2e08b05c6e2794b5b6f0d
joyantamishu <joyantamishu@users.noreply.github.com>
2024-10-08 20:53:40 -0400

    Update simple_example.cc

--- examples/simple_example.cc
+++ examples/simple_example.cc
@@ -4,7 +4,7 @@
 int main () {
 	RateLimitter* rt = RateLimitter::New(10,10);
   	for (int i=0;i<=11;i++) std::cout<<rt->Reserve(1)<<std::endl;
-  	std::cout<<"hello world\n";
+  	std::cout<<"Done Requests\n";
 
 	return 0;
 }
